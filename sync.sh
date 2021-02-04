ff45064ca5d51d8a02513f561d08fbedee79de14
Arthur Kazaryants <arthur@kazaryants.com>
2021-02-04 23:56:55 +0300

    Update actions

--- sync.sh
+++ sync.sh
@@ -10,28 +10,32 @@ export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l g
 #git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
 
 
-#!/bin/bash
+##!/bin/bash
 
 WORK_DIR=$(dirname $0)
 DEV_DIR=$WORK_DIR'/dev'
 BRANCH=$INPUT_SOURCE_BRANCH
-PROD_DIR=$WORK_DIR'/prod'
+PROD_DIR='prod'
 
 ##########
+pwd
 
-cd $WORK_DIR
+cd ..
+
 mkdir $PROD_DIR
+cd $PROD_DIR
+
+pwd
+
 git clone $INPUT_TARGET_REPO_URL
 
 last_version=$(awk 'git log -1 --pretty=%B')
 last_version=$(awk  'BEGIN{FS=OFS="."}{$NF++}1' $last_version)
+echo $last_version
 
-
-cd $DEV_DIR
 #git pull origin ${BRANCH}
-rsync -r --exclude=.git . ${PROD_DIR}
+rsync -r --exclude=.git $WORK_DIR .
 
-cd $PROD_DIR
 
 git add .
 git status
