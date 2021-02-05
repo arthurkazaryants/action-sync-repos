409de40d7447588f37adb418fa34cfa379419d30
Arthur Kazaryants <arthur@kazaryants.com>
2021-02-05 22:40:36 +0300

    Update actions

--- sync.sh
+++ sync.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -eu
 
+echo 'STARTED'
+
 mkdir -p ~/.ssh
 echo "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
@@ -13,11 +15,12 @@ export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l g
 ##!/bin/bash
 
 WORK_DIR=$(dirname $0)
-DEV_DIR=$WORK_DIR'/dev'
+#DEV_DIR=$WORK_DIR'/dev'
 BRANCH=$INPUT_SOURCE_BRANCH
 PROD_DIR='prod'
 
 ##########
+echo 'Current dir: '
 pwd
 
 cd ..
@@ -25,18 +28,21 @@ cd ..
 mkdir $PROD_DIR
 cd $PROD_DIR
 
+echo 'Current dir: '
 pwd
 
 git clone $INPUT_TARGET_REPO_URL
 
 last_version=$(awk 'git log -1 --pretty=%B')
 last_version=$(awk  'BEGIN{FS=OFS="."}{$NF++}1' $last_version)
+echo 'Last Version: '
 echo $last_version
 
 #git pull origin ${BRANCH}
+echo 'Rsync: '
 rsync -r --exclude=.git $WORK_DIR .
 
-
+echo 'Git add '
 git add .
 git status
 
